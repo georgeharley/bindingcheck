8ce3699479b2b09256e4b6155744dab0ad83eac4
gharley <gharley@pivotal.io>
2017-05-25 17:13:33 +0100

    Initial version

---
+++ src/main/java/io/pivotal/gharley/BindingcheckApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.gharley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableCircuitBreaker
+public class BindingcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BindingcheckApplication.class, args);
+	}
+}
---
+++ src/test/java/io/pivotal/gharley/BindingcheckApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.gharley;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BindingcheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
