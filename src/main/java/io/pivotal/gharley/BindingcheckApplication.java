501914328d058c7f8fb2a6ea689b3b69fccffa78
gharley <gharley@pivotal.io>
2017-05-25 17:50:59 +0100

    Update to make dependencies issue easier to see

--- src/main/java/io/pivotal/gharley/BindingcheckApplication.java
+++ src/main/java/io/pivotal/gharley/BindingcheckApplication.java
@@ -2,9 +2,10 @@ package io.pivotal.gharley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
 public class BindingcheckApplication {
 
 	public static void main(String[] args) {
